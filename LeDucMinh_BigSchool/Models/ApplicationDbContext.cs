c3485b78a6693aa04b26d473d15b8e4c19211263
PC <PC@LAPTOP-C03C5NQC>
2022-05-30 16:45:09 +0700

    Add project files.

---
+++ LeDucMinh_BigSchool/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace LeDucMinh_BigSchool.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ LeDucMinh_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LeDucMinh_BigSchool.Startup))]
+namespace LeDucMinh_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
